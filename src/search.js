0c4739521fb857335086161a3188dc2147773886
Hyunjung Lee <trlee329@gmail.com>
2017-05-09 11:56:00 +0200

    search

---
+++ src/search.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+export class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.onInput = this.onInput.bind(this);
+        
+    }
+
+
+    onInput(e) {
+        var obj = {};
+        obj[e.target.id] = e.target.value;
+        this.setState(obj);
+    }
+
+    render() {
+        let elem = (
+            <div className="searchBox">
+                <input onChange={this.onInput} id="search" />
+                <button onClick={this.search}> search </button>
+            </div>
+        );
+
+        return elem;
+    }
+}
