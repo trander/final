5a5fd987cf50cf4d3327534aae2d9ee2298d2b3e
Hyunjung Lee <trlee329@gmail.com>
2017-05-11 10:27:51 +0200

    search example

--- src/searchAPI.js
+++ src/searchAPI.js
@@ -1,3 +1,27 @@
+// Imports the Google Cloud client library
 const Translate = require('@google-cloud/translate');
 
+// Your Google Cloud Platform project ID
 const projectId = 'skillful-garden-149102';
+
+// Instantiates a client
+const translateClient = Translate({
+  projectId: projectId
+});
+
+// The text to translate
+const text = 'Hello, world!';
+// The target language
+const target = 'ru';
+
+// Translates some text into Russian
+translateClient.translate(text, target)
+  .then((results) => {
+    const translation = results[0];
+
+    console.log(`Text: ${text}`);
+    console.log(`Translation: ${translation}`);
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
