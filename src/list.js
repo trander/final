ea6e7add55001020a20764ac63f33a9e372d3bc4
hyunjung lee <hyunjung lee>
2017-06-08 15:27:57 +0200

    voca list

--- src/list.js
+++ src/list.js
@@ -2,16 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class List extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
-        this.state= {};
+        this.state = {};
     }
 
     render() {
-        return(
+        let elem = (
             <div>
-                "List of daily vocabulary"
+                <div id="daily"> Board </div>
             </div>
         );
+
+        return elem;
     }
 }
+
+
+// let elem = (
+//     <div>
+//         <div className="daily_board">
+//             <div>
+//                 <div className="daily">12-05-2017
+//                     <div className="vocalist">
+//                         <ul></ul>
+//                         <li><input type="checkbox"/> voca 1</li>
+//                         <li><input type="checkbox"/> voca 2</li>
+//                         <li><input type="checkbox"/> voca 3</li>
+//                         <li><input type="checkbox"/> voca 4</li>
+//                         <li><input type="checkbox"/> voca 5</li>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="daily">11-05-2017
+//                 <div className="vocalist">
+//                     <ul></ul>
+//                     <li><input type="checkbox"/> voca 1</li>
+//                     <li><input type="checkbox"/> voca 2</li>
+//                     <li><input type="checkbox"/> voca 3</li>
+//                     <li><input type="checkbox"/> voca 4</li>
+//                 </div>
+//             </div>
+//             <div className="daily">10-05-2017
+//                 <div className="vocalist">
+//                     <ul></ul>
+//                     <li><input type="checkbox"/> voca 1</li>
+//                     <li><input type="checkbox"/> voca 2</li>
+//                     <li><input type="checkbox"/> voca 3</li>
+//                     <li><input type="checkbox"/> voca 4</li>
+//                     <li><input type="checkbox"/> voca 5</li>
+//                     <li><input type="checkbox"/> voca 6</li>
+//                     <li><input type="checkbox"/> voca 7</li>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// );
